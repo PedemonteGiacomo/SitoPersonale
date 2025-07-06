d4deb2af0cc206168dcb0f0d33201631d7765855
Giacomo Pedemonte <74009749+PedemonteGiacomo@users.noreply.github.com>
2025-07-06 12:46:46 +0200

    Add React starfield background

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Starfield from './components/Starfield';
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Starfield />
+      <div id="content">
+        {/* Add your site content here */}
+      </div>
+    </>
+  );
+};
+
+export default App;
